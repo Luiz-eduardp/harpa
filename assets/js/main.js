af3bbcc2c97d3525aa5549ae719a4f08c4d373fb
Luiz-eduardp <luizzeduardp@gmail.com>
2021-07-03 11:38:05 -0300

    +versiculos

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,41 @@ aos que são retos fica bem louvá-lo. Louvem o Senhor com harpa;
 ofereçam-lhe música com lira de dez cordas. Cantem-lhe uma nova canção;
 toquem com habilidade ao aclamá-lo <br> <br><b>
 Salmos 33:1-3</b>`
-const facts = [versi1, versi2, versi3];
+const versi5 = `falando entre vocês com salmos, hinos e cânticos espirituais, cantando e louvando de coração ao Senhor,
+<br><br>Efésios 5:19</b>`
+const versi6 = ` Habite ricamente em vocês a palavra de Cristo; ensinem e aconselhem-se uns aos outros com toda a sabedoria e cantem salmos, hinos e cânticos espirituais com gratidão a Deus em seu coração.
+<br><br>Colossenses 3:16
+</b>
+`
+const versi7 = ` Então Miriã, a profeti­sa, irmã de Arão, pegou um tamborim e todas as mulheres a seguiram, tocando tamborins e dançando. E Miriã lhes respondia, cantando:
+"Cantem ao Senhor,
+pois triunfou gloriosamente.
+Lançou ao mar o cavalo
+e o seu cavaleiro".<br><br>
+Êxodo 15:20-21
+</b>
+`
+const versi8 = ` "Ouçam, ó reis!
+Governantes, escutem!
+Cantarei ao Senhor, cantarei;
+comporei músicas ao Senhor,
+o Deus de Israel.<br> <br>
+Juízes 5:3
+</b>
+`
+const versi9 = ` Davi e todos os israelitas iam cantando e dançando perante o Senhor, ao som de todo tipo de instrumentos de pinho: harpas, liras, tamborins, chocalhos e címbalos.
+<br><br>2 Samuel 6:5
+</b>
+`
+const versi10 = ` Sempre que o espírito mandado por Deus se apoderava de Saul, Davi apanhava sua harpa e tocava. Então Saul sentia alívio e melhorava, e o espírito maligno o deixava.
+<br><br>1 Samuel 16:23
+</b>
+`
+
+
+
+
+const facts = [versi1, versi2, versi3, versi4, versi5, versi6, versi7, versi8, versi9, versi10];
 
 ! function newFact() {
     const randomFact = Math.floor(Math.random() * facts.length);
